16f8db3f2022067832a25ccb853147e743bd9d4e
Shobhit Juglan <shojug123@gmail.com>
2023-04-14 13:26:19 +0530

    Updated Code to link Google Sheet with the app

--- app/src/main/java/com/parivishti/racetime/MainActivity.java
+++ app/src/main/java/com/parivishti/racetime/MainActivity.java
@@ -1,16 +1,30 @@
 package com.parivishti.racetime;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_scan;
@@ -43,7 +57,13 @@ public class MainActivity extends AppCompatActivity {
         {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Result");
-            builder.setMessage(result.getContents());
+            String name = new String("");
+            String entry_no = new String("");
+            String[] arrOfStr = result.getContents().split(":", 2);
+            name = arrOfStr[0];
+            entry_no = arrOfStr[1];
+            String msg = new String(name+":"+entry_no);
+            builder.setMessage(msg);
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
             {
                 @Override
@@ -52,8 +72,52 @@ public class MainActivity extends AppCompatActivity {
                     dialogInterface.dismiss();
                 }
             }).show();
-            System.out.println(result.getContents());
+            String finalName = name;
+            String finalEntry_no = entry_no;
+            btn_scan.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    addItemToSheet(finalName, finalEntry_no);
+                }
+            });
+
         }
     });
+    private void addItemToSheet(String name, String entry_no){
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbyopFLDEtCAYc8mUu2pmbbBKIsTQ2ZckkWZHMd7iJTvJYSx3VeAn9swTTC8ig55qzep/exec", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, ""+response, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String , String> params = new HashMap<>();
+                params.put("action","addItem");
+                params.put("name",name);
+                params.put("entryNumber",entry_no);
+
+                return params;
+            }
+        };
+
+        int timeOut = 50000;
+
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(timeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(retryPolicy);
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        queue.add(stringRequest);
+
+    }
 }
