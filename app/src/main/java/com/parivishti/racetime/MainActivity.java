b72852b8b94ec3b683740ddb00bd4704400b3b07
Shobhit Juglan <shojug123@gmail.com>
2023-04-15 20:20:42 +0530

    working app

--- app/src/main/java/com/parivishti/racetime/MainActivity.java
+++ app/src/main/java/com/parivishti/racetime/MainActivity.java
@@ -1,34 +1,9 @@
 package com.parivishti.racetime;
 
-//import android.app.ProgressDialog;
-//import android.os.Bundle;
-//import android.widget.TextView;
-//
-//import android.widget.Button;
-//import android.widget.Toast;
-//
-//import androidx.activity.result.ActivityResultLauncher;
-//import androidx.annotation.Nullable;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.android.volley.AuthFailureError;
-//import com.android.volley.DefaultRetryPolicy;
-//import com.android.volley.Request;
-//import com.android.volley.RequestQueue;
-//import com.android.volley.Response;
-//import com.android.volley.RetryPolicy;
-//import com.android.volley.VolleyError;
-//import com.android.volley.toolbox.StringRequest;
-//import com.android.volley.toolbox.Volley;
-//import com.journeyapps.barcodescanner.ScanContract;
-//import com.journeyapps.barcodescanner.ScanOptions;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -52,50 +27,69 @@ import com.journeyapps.barcodescanner.ScanOptions;
 import java.util.HashMap;
 import java.util.Map;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-    Button btn_scan, btn_start, btn_stop, btn_reset;
-
-    TextView tv_timer;
-
-    private boolean isTimerRunning = false;
-    private long startTimeInMillis;
-    private long timeLeftInMillis;
-    private long endTime;
+    private TextView timerTextView;
+    private Button startButton, resetButton, scanButton;
+    private long startTime = 0L;
+    private Handler customHandler = new Handler();
+    private long timeInMilliseconds = 0L;
+    private long timeSwapBuff = 0L;
+    private long updatedTime = 0L;
 
 
-    private Handler handler = new Handler();
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            timeLeftInMillis = endTime - System.currentTimeMillis();
-
-            if (timeLeftInMillis < 0) {
-                isTimerRunning = false;
-                timeLeftInMillis = 0;
-                updateTimerText();
-            } else {
-                updateTimerText();
-                handler.postDelayed(this, 1000);
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_scan = findViewById(R.id.btn_scan);
-        btn_start = findViewById(R.id.btn_start);
-        btn_stop = findViewById(R.id.btn_stop);
-        btn_reset = findViewById(R.id.btn_reset);
-        tv_timer = findViewById(R.id.tv_timer);
-        btn_scan.setOnClickListener(v ->{scanCode();});
-        btn_start.setOnClickListener(v -> startTimer());
-        btn_stop.setOnClickListener(v -> stopTimer());
-        btn_reset.setOnClickListener(v -> resetTimer());
+        timerTextView = findViewById(R.id.tv_timer);
+        startButton = findViewById(R.id.btn_start);
+        resetButton = findViewById(R.id.btn_reset);
+        scanButton = findViewById(R.id.btn_scan);
+
+        startButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
+        scanButton.setOnClickListener(this);
+        resetButton.setEnabled(false);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                startTime = System.currentTimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+                startButton.setEnabled(false);
+                resetButton.setEnabled(true);
+                break;
+            case R.id.btn_reset:
+                timeSwapBuff = 0L;
+                startTime = System.currentTimeMillis();
+                customHandler.removeCallbacks(updateTimerThread);
+                timerTextView.setText("00:00:00.0");
+                startButton.setEnabled(true);
+                resetButton.setEnabled(false);
+                break;
+            case R.id.btn_scan:
+                scanCode();
+        }
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = System.currentTimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) ((updatedTime / 100)%10);
+            int hours = mins / 60 ;
+            timerTextView.setText("" + String.format("%02d", hours) + ":" + String.format("%02d", mins) + ":" + String.format("%02d", secs)+"."+String.format("%01d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+    };
 
     private void scanCode() {
         ScanOptions options = new ScanOptions();
@@ -106,43 +100,6 @@ public class MainActivity extends AppCompatActivity {
         barLauncher.launch(options);
     }
 
-    private void startTimer() {
-        if (!isTimerRunning) {
-            startTimeInMillis = System.currentTimeMillis();
-            endTime = startTimeInMillis + timeLeftInMillis;
-
-            handler.postDelayed(runnable, 1000);
-            isTimerRunning = true;
-        }
-    }
-
-    private void stopTimer() {
-        if (isTimerRunning) {
-            handler.removeCallbacks(runnable);
-            timeLeftInMillis = endTime - System.currentTimeMillis();
-            isTimerRunning = false;
-        }
-    }
-
-    private void resetTimer() {
-        if (isTimerRunning) {
-            handler.removeCallbacks(runnable);
-            isTimerRunning = false;
-        }
-
-        timeLeftInMillis = 0;
-        updateTimerText();
-    }
-
-    private void updateTimerText() {
-        int minutes = (int) (timeLeftInMillis / 1000) / 60;
-        int seconds = (int) (timeLeftInMillis / 1000) % 60;
-
-        String timeLeftFormatted = String.format("%02d:%02d", minutes, seconds);
-        tv_timer.setText(timeLeftFormatted);
-    }
-
-
 
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->
     {
@@ -150,13 +107,21 @@ public class MainActivity extends AppCompatActivity {
             String[] arrOfStr = result.getContents().split(":", 2);
             String name = arrOfStr[0];
             String entry_no = arrOfStr[1];
-            addItemToSheet(name, entry_no);
+            timeInMilliseconds = System.currentTimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) ((updatedTime / 100)%10);
+            int hours = mins / 60 ;
+            String time = "" + String.format("%02d", hours) + ":" + String.format("%02d", mins) + ":" + String.format("%02d", secs)+"."+String.format("%01d", milliseconds);
+            addItemToSheet(name, entry_no, time);
         }
     });
 
-    private void addItemToSheet(String name, String entry_no) {
+    private void addItemToSheet(String name, String entry_no, String time) {
         final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "Adding Item", "please wait...");
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbyopFLDEtCAYc8mUu2pmbbBKIsTQ2ZckkWZHMd7iJTvJYSx3VeAn9swTTC8ig55qzep/exec", new Response.Listener<String>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbw2n9YiHWmROWUB6afdkdUSKA0hagA569t_N1fMthwuA-g7YYe88kjmiLhc_rFraSt3/exec", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 dialog.dismiss();
@@ -175,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 params.put("action", "addItem");
                 params.put("name", name);
                 params.put("entryNumber", entry_no);
+                params.put("timer", time);
                 return params;
             }
         };
